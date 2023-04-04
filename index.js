6447ee3ab0ec62503dd62f00afb52553ca09ea83
wissarutbw <118594249+wissarutbw@users.noreply.github.com>
2023-04-04 15:35:46 +0700

    test api with line

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo! Automotion Works')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+// app.all("/", (req, res) => {
+//   console.log("Just got a request!");
+//   res.send("Yo! Automotion Works");
+// });
+app.post('/linebot', (req, res) => {
+    console.log(req.body)
+    books.push(req.body)
+    res.status(201).json(req.body)
+  })
+app.listen(process.env.PORT || 3000);
